2b9b129e335f39199c181b51e87975db4e882ee5
Stephane Moreel <stef.ksp@gmail.com>
2025-10-15 16:36:53 +0200

    add url render

--- my-react-app/frontend/src/utils/constants.js
+++ my-react-app/frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 // frontend/src/utils/constants.js
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_URL ?? 'https://portfolio-stephane-moreel.onrender.com';
 
 export const API_ROUTES = {
   SKILLS: `${API_URL}/api/skills`,
