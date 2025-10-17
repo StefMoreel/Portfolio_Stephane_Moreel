3475c4b47fe7a825bf941c115ae51a903e00aa44
Stephane Moreel <stef.ksp@gmail.com>
2025-10-17 14:56:12 +0200

    fix public id

--- my-react-app/frontend/src/components/Hero.jsx
+++ my-react-app/frontend/src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
   const pdfURL = "/CV_STEPHANE_MOREEL 092025 V2.pdf";
 
   // le chemin Cloudinary (avec la version) devient le "publicId" pour le proxy
-  const photoPublicId = "v1760520215/photo_sm_portfolio_ztg9oc_k3ssqm.webp";
+  const photoPublicId = "photo_sm_portfolio_ztg9oc_k3ssqm.webp";
 
   const USE_PROXY = import.meta.env.VITE_USE_IMAGE_PROXY === "true";
 
