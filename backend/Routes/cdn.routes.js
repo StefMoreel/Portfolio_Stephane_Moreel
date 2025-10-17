3c69ea87e11766d60e39b7fa3bf7b6ce80e66519
Stephane Moreel <stef.ksp@gmail.com>
2025-10-17 16:33:32 +0200

    fix cdn.routes

--- backend/Routes/cdn.routes.js
+++ backend/Routes/cdn.routes.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const https = require('node:https');
-
 const router = express.Router();
 const CLOUD = process.env.CLOUDINARY_CLOUD_NAME;
 
-// /cdn?id=<publicId>&t=<transformations>
-router.get('/cdn', (req, res, next) => {
+// Route pour /cdn/<transformations>/<publicId>
+router.get(/^\/cdn\/(.+?)\/(.+)/, (req, res, next) => {
   try {
-    const publicId = req.query.id;            // ex: "folder/sub/image_xxx"
-    const t = req.query.t || 'f_auto,q_auto,dpr_auto,c_fit,w_96,h_96';
+    const transformations = req.params[0];
+    const publicId = req.params[1];
+
     if (!CLOUD || !publicId) return res.sendStatus(400);
 
-    // le front aura fait encodeURIComponent → on decode ici
-    const decoded = decodeURIComponent(publicId);
-
-    const url = `https://res.cloudinary.com/${CLOUD}/image/upload/${t}/${decoded}`;
+    const url = `https://res.cloudinary.com/${CLOUD}/image/upload/${transformations}/${publicId}`;
 
     https.get(url, (up) => {
       res.setHeader('Content-Type', up.headers['content-type'] || 'image/jpeg');
